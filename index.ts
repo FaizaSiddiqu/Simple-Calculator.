fa490e27bc6ad6733e497ca175f71ddfde9aab76
[Faiza Siddiqui] <[faizasiddiqui902@gmail.com]>
2024-03-20 23:08:37 +0500

    push my calculator to gitub

---
+++ index.ts
@@ -0,0 +1,36 @@
+#! /esr/bin/env node
+import inquirer from "inquirer";
+
+const answer = await inquirer.prompt([
+  { message: "Enter First Number", type: "number", name: "FirstNumber" },
+  { message: " Enter second number", type: "number", name: "secondNumber" },
+  {
+    message: "Select one of the operation to perform action",
+    type: "list",
+    name: "operator",
+    choices: [
+      "Addition",
+      "Subtraction",
+      "Multiplication",
+      "Division",
+      "Exponentiation",
+      "modulus",
+      "BMI calculator",
+    ],
+  },
+]);
+
+//Conditional Statement
+if (answer.operator === "Addition") {
+    console.log(answer.FirstNumber + answer.secondNumber);
+} else if (answer.operator === "Subtraction") {
+    console.log(answer.FirstNumber - answer.secondNumber);
+} else if (answer.operator === "Multiplication") {
+    console.log(answer.FirstNumber * answer.secondNumber);
+} else if (answer.operator === "Division") {
+    console.log(answer.FirstNumber / answer.secondNumber);
+} else {
+    console.log("please select a valid operator")
+}
+
+ 
